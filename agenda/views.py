7a954c01d7993984b93b2e74489655067af06026
Felipe Dutra <dutrafelipe77@gmail.com>
2025-04-22 20:32:03 -0300

    feat: updating serializers (agenda, consultas)

--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
+from agenda.models import Agenda
+from agenda.serializers import AgendaSerializer 
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+class AgendaViewSet(viewsets.ModelViewSet):
+    queryset = Agenda.objects.all()
+    serializer_class = AgendaSerializer
+    #permission_classes = [IsAuthenticated]  # Você pode customizar depois com IsAdminUser ou uma permissão própria
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['dia_semana', 'dentista']
+    search_fields = ['dentista__nome']
+    ordering_fields = ['dia_semana', 'hora_inicio', 'hora_fim']
+    ordering = ['dia_semana', 'hora_inicio']
